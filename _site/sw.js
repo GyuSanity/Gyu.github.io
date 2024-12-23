bc269269d6083aab5fa9e39fab8d3eb0d0a6b68c
GyuSanity <slipperyeely@gmail.com>
2024-12-23 21:05:31 +0900

    Deploy Jekyll site

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241223.205027';
+const cacheName = 'chirpy-20241223.210216';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
